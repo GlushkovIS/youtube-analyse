1af1e6972c3a0a49d5e1109d780f309e3086634e
IS Glushkov <53116914+GlushkovIS@users.noreply.github.com>
2020-09-19 17:55:45 +0300

    1

--- src/Model/YouTubeChannelModel.php
+++ src/Model/YouTubeChannelModel.php
@@ -3,7 +3,6 @@
 
 namespace Model;
 
-
 use Controllers\ElasticSearchController;
 
 class YouTubeChannelModel
--- src/View/MainView.php
+++ src/View/MainView.php
@@ -1,3 +1,4 @@
+<?php include_once 'src/Controllers/MainViewController.php'?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -6,7 +7,7 @@
 </head>
 <body>
 <h3>Введите наименование канала для добавления в статистику:</h3>
-<form action="/src/Controllers/MainViewController.php" method="post">
+<form action="/" method="post">
     <input type="text" name="nameChannel" placeholder="наименование канала">
     <input type="submit" name="submit" placeholder="отправить">
 </form>
